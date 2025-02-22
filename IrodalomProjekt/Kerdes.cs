564a7a491f053ee68a332318d8a71b2fe5f28e67
Roli <tothr@kkszki.hu>
2025-02-22 18:53:47 +0100

    Kérdés osztály megcsinálva

---
+++ IrodalomProjekt/Kerdes.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IrodalomProjekt
+{
+    internal class Kerdes
+    {
+        public string KerdesSzoveg { get; set; }
+
+        public string ValaszA { get; set; }
+
+        public string ValaszB { get; set; }
+
+        public string ValaszC { get; set; }
+
+        public string HelyesValasz { get; set; }
+
+        public string FelhasznaloValasza { get; set; }
+    }
+}
