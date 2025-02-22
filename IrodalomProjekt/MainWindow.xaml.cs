a6ce2acb8c3f26e17e065af8dadf77ab2211c3c5
Roli <tothr@kkszki.hu>
2025-02-22 19:00:32 +0100

    MutatKerdes implementálva

--- IrodalomProjekt/MainWindow.xaml.cs
+++ IrodalomProjekt/MainWindow.xaml.cs
@@ -74,6 +74,11 @@ namespace IrodalomProjekt
             }
         }
 
+        private void MutatKerdes(int aktualisIndex)
+        {
+            throw new NotImplementedException();
+        }
+
         private void KiertekelesClick(object sender, RoutedEventArgs e)
         {
 
